f8ae5c9a1fc8f3bd6be955adc30a3842a9403885
Raymond Chen <ahrchen@gmail.com>
2022-02-19 20:06:27 -0800

    Initial Commit

---
+++ LearnToMultiply/LearnToMultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnToMultiplyApp.swift
+//  LearnToMultiply
+//
+//  Created by Raymond Chen on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnToMultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
