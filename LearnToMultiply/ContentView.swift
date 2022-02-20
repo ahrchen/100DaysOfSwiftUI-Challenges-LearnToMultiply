15ae29756e38d31f6d7fda0b01c4dece5614a850
Raymond Chen <ahrchen@gmail.com>
2022-02-19 20:49:20 -0800

    Add questionBank

--- LearnToMultiply/ContentView.swift
+++ LearnToMultiply/ContentView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var questionBank = [[Int]]()
+    @State private var currentQuestions = [[Int]]()
+    
+    @State private var maxMultiples = 2
+    @State private var numQuestions = 5
+    @State private var start = false
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+        if start {
+            Text("start")
+        } else {
+            Text("selection")
+        }
+        
+            
+        }
+        .onAppear(perform: loadQuestions)
+        .navigationTitle("Learn Multiplications")
+    }
+        
+         
+    
+    func loadQuestions() {
+        for i in 2..<13 {
+            var temp_array = [Int]()
+            for j in 1 ..< 13 {
+                temp_array.append(i * j)
+            }
+            questionBank.append(temp_array)
+        }
+        for questionSet in questionBank {
+            print(questionSet)
+        }
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
